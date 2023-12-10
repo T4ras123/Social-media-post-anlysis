61c2e03b676003feec3bf5b25567c459afd93fd3
Vover <vovatara123@gmail.com>
2023-12-10 15:03:45 +0400

    Changed almost everything. Created new functions and optimised a little bit

--- Social_media_analysis.py
+++ Social_media_analysis.py
@@ -9,29 +9,99 @@ Most active country
 """
 
 
+def main():
+    inpt = get_user_input()
+    do_analysis(inpt)
+
+
+def show_data():
+    global splited_data, special_symbols, words, hashtags, \
+        links, header, test_data
+    with (open(r"D:\Vova\Education\Uni\Intro to CS\Project\tweets_raw.csv",
+               "r", encoding="utf8") as file):
+        data = list(csv.reader(file))
+        for i in range(6):
+            print([" ".join(element.split(' '))
+                   if element != '' else 'NoInfo'
+                   for element in data[i]])
+        print('--------------------------------------------')
+        for a in range(-1, -5):
+            print(a)
+            print([" ".join(element.split(' '))
+                   if element != '' else 'NoInfo'
+                   for element in data[a]])
+
+
+def every_word(length):
+    global words
+    with (open(r"D:\Vova\Education\Uni\Intro to CS\Project\tweets_raw.csv", "r", encoding="utf8") as file):
+        data = csv.reader(file)
+        for row in data:
+            all_words = row[1].split(" ")
+            while '' in all_words:
+                all_words.remove('')
+            for word in all_words:
+                if len(set(word).intersection(special_symbols)) == 0 and len(word) >= length:
+                    words.append(word.lower())
+
+
+def every_hashtag():
+    global hashtags
+    with (open(r"D:\Vova\Education\Uni\Intro to CS\Project\tweets_raw.csv", "r", encoding="utf8") as file):
+        data = list(csv.reader(file))
+        for row in data:
+            all_words = row[1].split(" ")
+            while '' in all_words:
+                all_words.remove('')
+            for word in all_words:
+                if word[0] == '#':
+                    hashtags.append(word)
+
+
+def open_file(length=0):
+    global splited_data, special_symbols, words, hashtags,\
+         header, test_data
+    with (open(r"D:\Vova\Education\Uni\Intro to CS\Project\tweets_raw.csv", "r", encoding="utf8") as file):
+        data = csv.reader(file)
+        for row in data:
+            splited_data.append([" ".join(element.split())
+                                 if element != '' else 'NoInfo'
+                                 for element in row][2:])
+
+    header = splited_data[0]
+    test_data = splited_data[1:][:10000]
+
+
 def get_user_input():
     global inputs
     user_input = input("What do you want to know?\n"
                        "A - The most popular words\n"
                        "B - the most popular hashtags\n"
                        "C - Popularity of the tweets\n"
-                       "E - exit")
+                       "S - Show data\n"
+                       "E - exit\t")
     if user_input.lower() not in inputs:
         print("invalid input")
         get_user_input()
+    else:
+        return user_input.lower()
+
+
+def do_analysis(user_input):
+    if user_input == 'e':
+        print("okay, bye")
+        exit()
     elif user_input.lower() == 'a':
-        number, length = int(input("How many top words?")), int(input("Minimum word length"))
+        number = int(input("\nHow many top words?\n"))
+        length = int(input("\nMinimum word length\n"))
         find_words(number, length)
     elif user_input.lower() == 'b':
-        number = int(input("How many top hashtags?"))
+        number = int(input("\nHow many top hashtags?\n"))
         find_hashtags(number)
     elif user_input.lower() == 'c':
         popular_tweets()
-    else:
-        print("okay")
-        exit()
-
-
+    elif user_input.lower() == 's':
+        show_data()
 
 def draw_pie(values, names):
     plt.style.use('dark_background')
@@ -51,38 +121,29 @@ def draw_bars(x_axis, y_axis, x_name='', y_name="", title=""):
 
 
 def find_hashtags(top_n):
-    global test_data
+    global hashtags
+    every_hashtag()
     u, w = [], []
-    hashtags = []
-    for tweet in test_data:
-        all_words = tweet[0].split(' ')
-        for word in all_words:
-            if word[0] == '#':
-                hashtags.append(word)
-
     p = Counter(hashtags).most_common(top_n)
     for i in p:
         w.append(i[0])
         u.append(i[1])
+    w.append('Else')
+    u.append(len(hashtags)-len(u))
     draw_pie(u, w)
 
 
 def find_words(top_m, length=5):
-    global test_data
-    words = []
+    global words
+    every_word(length)
     w = []
     u = []
-    for tweet in test_data:
-        all_words = tweet[0].split(' ')
-        for word in all_words:
-            if (len(set(word).intersection(special_symbols)) == 0 and
-                    len(word) >= length):
-                words.append(word.lower())
     p = Counter(words).most_common(top_m)
     for i in p:
         w.append(i[0])
         u.append(i[1])
-
+    w.append('Else')
+    u.append(len(words)-len(u))
     draw_pie(u, w)
 
 
@@ -118,11 +179,12 @@ def most_active_places():
 
 
 def popular_tweets():
+    open_file()
     engagement = []
     number_of_tweets = []
     global test_data
     for tweet in test_data:
-        eng = int(int(tweet[3])*2 + int(tweet[4]))
+        eng = int(int(tweet[2])*2 + int(tweet[3]))
         engagement.append(eng)
     srt_eng = set(engagement)
     for i in srt_eng:
@@ -131,25 +193,14 @@ def popular_tweets():
               "Number of tweets", "Engagement to tweets ratio")
 
 
-splited_data = []
-
-
-with open(r"D:\Vova\Education\Uni\Intro to CS\Project\tweets_raw.csv", "r", encoding="utf8") as file:
-    data = csv.reader(file)
-    for row in data:
-        splited_data.append([" ".join(element.split())
-                             if element != '' else 'NoInfo'
-                             for element in row][2:])
-    
-
-header = splited_data[0]
-
-dataset = splited_data[1:]
-
-test_data = dataset[1:100000]
-
 special_symbols = r'!@"#№$;:%^&?/\|*()_-+=><.,{}[]~`'
 
-inputs = ['a', 'b', 'c', 'e']
+inputs = ['a', 'b', 'c', 'e', 's']
+hashtags = []
+words = []
+splited_data = []
+header = []
+test_data = []
+main()
 
-get_user_input()
+#%%
